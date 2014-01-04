85ac69c6dac1bb0eb397fdbd12b25da4ade1077f
Pierre Guillemot <pierreguillemot@yahoo.fr>
2014-01-04 18:32:22 +0800

    Updated the item part

--- js/app/models/actionItem.js
+++ js/app/models/actionItem.js
@@ -1,47 +1,65 @@
-define(["models/item"], function(mItem){
+/**
+ * Inherits of Item, represents an item you can use like a key or
+ * a torch for instance.
+ * @class ActionItem
+ * @author Pierre Guillemot
+ */
+define(["models/item"], function (mItem) {
 
-    function ActionItem(id, name, desc, value, opts){
-        mItem.Item.call(this, id, name, desc, value);
-        
-        //New field
-        this.setOpts(opts);
-    }
-    
-    ActionItem.prototype = Object.create(mItem.Item.prototype);
-    
-    ActionItem.prototype.constructor = ActionItem;
-
-    ActionItem.prototype.setOpts = function setOpts(opts){
-        this.bonuses = new Array();
-        
-        if(typeof opts !== "undefined"){
+    function ActionItem(_id, _name, _description, _value, _opts) {
+        mItem.Item.call(this, _id, _name, _description, _value);
+       
+        //Bonuses given by the item 
+        this.bonuses = [];
+            
+        if (_opts !== undefined) {
         
             //Bonuses
-            if(typeof opts.bonuses !== "undefined"){
+            if (_opts.bonuses !== undefined) {
                 var self = this;
-                opts.bonuses.forEach(function(item){
-                    self.bonuses.push(item);  
+                _opts.bonuses.forEach(function (item) {
+                    self.bonuses.push(item);
                 });
             }
             
             //Position constraint
-            this.constraint = opts.constraint;
+            this.constraint = _opts.constraint;
         }
-    };
+    }
     
-    ActionItem.prototype.use = function use(source, target){
-        this.bonuses.forEach(function(item){
-            target.update(item);  
+    ActionItem.prototype = Object.create(mItem.Item.prototype);
+    ActionItem.prototype.constructor = ActionItem;
+   
+    /**
+     * Uses an action itemm needs a source and a target.
+     * e.g: the hero uses a key on a door, or a torch in a room
+     * @method use
+     * @param {Object} _source Creature type
+     * TODO: check the _target parameter
+     * @param {Object} _target Not sure anymore ...
+     * @return {Nothing}
+     * @public
+     */
+    ActionItem.prototype.use = function (_source, _target) {
+        this.bonuses.forEach(function (item) {
+            _target.update(item);
         });
         
-        var out = source.name + " used " + this.name;
-        if(source !== target)
-            out += " on " + target.name; 
-        
+        var out = _source.getName() + " used " + this.getName();
+        if (_source !== _target) {
+            out += " on " + _target.getName();
+        }
+
         return out;
     };
-    
-    ActionItem.prototype.toJson = function toJson(){
+   
+    /**
+     * Returns a JSON representation of the ActionItem
+     * @method toJson
+     * @return {String} a JSON representation of the action item
+     * @public
+     */
+    ActionItem.prototype.toJson = function () {
         return JSON.stringify(this);
     };
     
--- js/app/models/item.js
+++ js/app/models/item.js
@@ -1,15 +1,104 @@
-//Only to be inherited by child classes
-define([], function(){
+/**
+ * Base Item class
+ * @class Item
+ * @author Pierre Guillemot
+ */
+define([], function () {
 
-    function Item(id, name, desc, value){
-        this.id = id;
-        this.name = name;
-        this.description = desc;
-        this.value = value;
+    /**
+     * Constructor
+     * @method Item
+     * @param {Number} _id          Id of the item 
+     * @param {String} _name        name of the item
+     * @param {String} _description description of the item
+     * @param {Number} _value       value of the item
+     * @return {Nothing}
+     * @public
+     */
+    function Item(_id, _name, _description, _value) {
+        /**
+         * Unique identifier of the item
+         */
+        var id;
+
+        /**
+         * Name of the item
+         */
+        var name;
+
+        /**
+         * Description of the item, is optionnal
+         */
+        var description;
+
+        /**
+         * Value of the item
+         */
+        var value;
+
+        /**
+         * Getter for id
+         * @method getId
+         * @return {Number} the ID of the item
+         * @public
+         */
+        this.getId = function () {
+            return id;
+        };
+
+        /**
+         * Getter for name
+         * @method getName
+         * @return {String} the name of the item
+         * @public
+         */
+        this.getName = function () {
+            return name;
+        };
+
+        /**
+         * Getter for description
+         * @method getDescription
+         * @return {String} the description of the item
+         * @public
+         */
+        this.getDescription = function () {
+            return description;
+        };
+
+        /**
+         * Getter for value
+         * @method getValue
+         * @return {Number} the value of the item
+         * @public
+         */
+        this.getValue = function () {
+            return value;
+        };
+
+        //TODO: use setters
+        id = _id;
+        name = _name;
+        description = _description;
+        value = _value;
     }
-    
-    Item.prototype.toString = function toString(){
-        return "[" + this.id + "] " + this.name + " : " + this.description + " Worth " + this.value + " gold coins.";
+   
+    /**
+     * Return a string representation of the Item
+     * @method toString
+     * @return {String} id, name, description and value of the item
+     * @public
+     */
+    Item.prototype.toString = function () {
+        return "[" +
+            this.getId() +
+            "] " +
+            this.getName() +
+            " : " +
+            this.getDescription() +
+            " Worth " +
+            this.getValue() +
+            " gold coins.";
     };
     
     return {Item : Item};
