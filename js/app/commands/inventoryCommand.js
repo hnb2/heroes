4213c1458a4a1e63280158b330c90c7746b4168e
Pierre Guillemot <pierreguillemot@yahoo.fr>
2014-01-12 20:37:03 +0800

    Fixed the inventory command

--- js/app/commands/inventoryCommand.js
+++ js/app/commands/inventoryCommand.js
@@ -1,9 +1,22 @@
-define(["commands/command"], function(mCommand){
-    function InventoryCommand(){
+/**
+ * "inventory" returns information about all the items in the
+ * bag of the hero.
+ * @class InventoryCommand
+ * @author Pierre Guillemot
+ */
+define(["commands/command"], function (mCommand) {
+
+    /**
+     * Constructor
+     * @method InventoryCommand
+     * @return {Nothing}
+     * @public
+     */
+    function InventoryCommand() {
         mCommand.Command.call(this,
-            'inventory', 
-            'Check your bag', 
-            undefined, 
+            'inventory',
+            'Check your bag',
+            undefined,
             'dom'
         );
     }
@@ -11,23 +24,38 @@ define(["commands/command"], function(mCommand){
     InventoryCommand.prototype = Object.create(mCommand.Command.prototype);
     InventoryCommand.prototype.constructor = InventoryCommand;
     
-    InventoryCommand.prototype.exec = function(args, context){
+    /**
+     * Returns information about all the items in the inventory
+     * of the hero.
+     * @method exec
+     * @param {Array}  _args    is empty here
+     * @param {Object} _context contains the environment
+     * @return {Object} view object
+     * @public
+     */
+    InventoryCommand.prototype.exec = function (_args, _context) {
         //Creating a "shortcut"
-        var env = context.environment;
-            
+        var env = _context.environment;
+        
+        //Get the content of the inventory of the hero    
         var bag = env.hero.getInventory();
         
-        var dom = new Array();
+        var dom = [];
 
-        if(bag.length > 0){
-            bag.forEach(function(item){
-                dom.push( env.domHelper.createText( "info", item.toString() ) );
+        if (bag.length > 0) {
+            bag.forEach(function (_item) {
+                dom.push(
+                    env.domHelper.createText(
+                        "info",
+                        _item.toString()
+                    )
+                );
             });
+
             return env.domHelper.appendArray(dom);
         }
-        else{
-            return env.domHelper.createText("error", "Empty bag...");
-        }
+
+        return env.domHelper.createText("error", "Empty bag...");
     };
     
     return {InventoryCommand: InventoryCommand};
